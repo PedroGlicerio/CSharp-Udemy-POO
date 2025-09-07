6d3950dbd84082c0e9f7d470a810c9ae24f4f648
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-07 14:12:30 -0300

    Resolução do Exercício 03 - Estruturas Condicionais

---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio03/Entities/Diferenca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio03.Entities {
+    internal class Diferenca {
+
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+        public int Valor3 { get; set; }
+        public int Valor4 { get; set; }
+
+        public Diferenca(int valor1, int valor2, int valor3, int valor4) {
+
+            Valor1 = valor1;
+            Valor2 = valor2;
+            Valor3 = valor3;
+            Valor4 = valor4;
+        }
+
+        public double Calculo() {
+
+            return (Valor1 * Valor2 - Valor3 * Valor4);         
+        }
+
+        public override string ToString() {
+            return $"Diferença: {Calculo()}";
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Exercicio03.Entities;
+class Program {
+    public static void Main() {
+
+        Console.WriteLine("Digite 4 valores:");
+        Console.WriteLine();
+        Console.Write("Primeiro valor: ");
+        int valor1 = int.Parse(Console.ReadLine());
+        Console.Write("Segundo valor: ");
+        int valor2 = int.Parse(Console.ReadLine());
+        Console.Write("Terceiro valor: ");
+        int valor3 = int.Parse(Console.ReadLine());
+        Console.Write("Quarto valor: ");
+        int valor4 = int.Parse(Console.ReadLine());
+
+        Diferenca d = new Diferenca(valor1, valor2, valor3, valor4);
+
+        Console.WriteLine();
+        Console.WriteLine(d);
+    }
+}
