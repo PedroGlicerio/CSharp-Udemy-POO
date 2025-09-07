7eeceddcf007e248b6e8ffd799fd739772435bbe
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-07 10:42:23 -0300

    Resolução do Exercício 01 - Estrutura Sequencial

---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio01/Exe001/Exe001/Entities/Calculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Exe001.Entities {
+    internal class Calculo {
+
+        public int Valor1 { get; set; }
+        public int Valor2 { get; set; }
+
+        public Calculo(int valor1, int valor2) {
+
+            Valor1 = valor1;
+            Valor2 = valor2;
+        }
+
+        public int Soma() {
+
+            return Valor1 + Valor2;
+        }
+
+        public override string ToString() {
+            return $"Resultado da soma: {Soma()}";
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio01/Exe001/Exe001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Exe001.Entities;
+class Program {
+    public static void Main() {
+
+        Console.Write("Digite o primeiro valor: ");
+        int valor1 = int.Parse(Console.ReadLine());
+        Console.Write("Digite o segundo valor: ");
+        int valor2 = int.Parse(Console.ReadLine());
+
+        Calculo soma = new Calculo(valor1, valor2);
+
+        Console.WriteLine();
+        Console.WriteLine(soma);
+    }
+}
