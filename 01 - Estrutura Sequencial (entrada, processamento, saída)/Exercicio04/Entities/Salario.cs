d366979bebef045b4cf1039050e7902fc994c3a6
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-07 14:39:36 -0300

    Resolução do Exercício 04 - Estrutura Sequencial

---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio04/Entities/Salario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercicio04.Entities {
+    internal class Salario {
+
+        public int Numero { get; set; }
+        public int Horas { get; set; }
+        public double Valor { get; set; }
+
+        public Salario(int numero, int horas, double valor) {
+
+            Numero = numero;
+            Horas = horas;
+            Valor = valor;
+        }
+
+        public double valorSalario() {
+
+            return Horas * Valor;
+        }
+
+        public override string ToString() {
+            return $"Número: {Numero}\nSalário: R${valorSalario():f2}";
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio04/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Exercicio04.Entities;
+class Program {
+    public static void Main() {
+
+        Console.Write("Número do funcionário: ");
+        int num = int.Parse(Console.ReadLine());
+        Console.Write("Horas trabalhadas: ");
+        int horas = int.Parse(Console.ReadLine());
+        Console.Write("Valor por hora: ");
+        double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Salario s = new Salario(num, horas, valor);
+
+        Console.WriteLine();
+        Console.WriteLine(s);
+    }
+}
