d7d32716d07942bef0849096df78108c44b9d6ae
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-07 12:06:27 -0300

    Resolução do Exercício 02 - Estruturas de Repetição

---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio02/Entities/Circulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio02.Entities {
+    internal class Circulo {
+
+        public double Raio { get; set; }
+
+        public Circulo(double raio) {
+
+            Raio = raio;
+        }
+
+        public double areaCirculo() {
+
+            double pi = 3.14159;
+            return pi * Math.Pow(Raio, 2);
+        }
+
+        public override string ToString() {
+            return $"Área: {areaCirculo():f4}";
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+using Exercicio02.Entities;
+class Program {
+    public static void Main() {
+
+        Console.Write("Digite o valor do raio: ");
+        double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Circulo r = new Circulo(raio);
+
+        Console.WriteLine();
+        Console.WriteLine(r);
+    }
+}
