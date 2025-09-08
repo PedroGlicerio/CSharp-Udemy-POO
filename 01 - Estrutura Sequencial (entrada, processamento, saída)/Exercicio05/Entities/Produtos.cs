c41471b56f1f916419a1045062aa80766b7b6b9d
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-08 11:59:43 -0300

    Resolução do Exercício 05 - Estrutura Sequencial

---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio05/Entities/Produtos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Exercicio05.Entities {
+    internal class Produtos {
+
+        public int Codigo { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+
+        public Produtos() {
+        }
+
+        public Produtos(int codigo, int quantidade, double valor) {
+
+            Codigo = codigo;
+            Quantidade = quantidade;
+            Valor = valor;
+        }
+
+        public double valorPagar() {
+
+            return Valor * Quantidade;
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Exercicio05.Entities;
+class Program {
+    public static void Main() {
+
+        Produtos p1 = new Produtos();
+
+        Console.WriteLine("Peça 1");
+        Console.Write("Digite o código, o números de peças e o valor unitário de cada peça: ");
+        string[] s1 = Console.ReadLine().Split(" ");
+        p1.Codigo = int.Parse(s1[0]);
+        p1.Quantidade = int.Parse(s1[1]);
+        p1.Valor = double.Parse(s1[2], CultureInfo.InvariantCulture);
+
+        Produtos p2 = new Produtos();
+
+        Console.WriteLine();
+        Console.WriteLine("Peça 2");
+        Console.Write("Digite o código, o números de peças e o valor unitário de cada peça: ");
+        string[] s2 = Console.ReadLine().Split(" ");
+        p2.Codigo = int.Parse(s2[0]);
+        p2.Quantidade = int.Parse(s2[1]);
+        p2.Valor = double.Parse(s2[2], CultureInfo.InvariantCulture);
+
+        Console.WriteLine();
+        double total = p1.valorPagar() + p2.valorPagar();
+        Console.WriteLine($"Valor a pagar: R${total:f2}");
+    }
+}
