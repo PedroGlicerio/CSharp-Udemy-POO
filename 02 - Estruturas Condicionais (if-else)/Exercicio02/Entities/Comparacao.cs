e0368d702492859d9e35521a8478b996b754b3ce
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-09 14:40:12 -0300

    Resolução do Exercício 02 - Estrutura Condicional (if-else)

---
+++ 02 - Estruturas Condicionais (if-else)/Exercicio02/Entities/Comparacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio02.Entities {
+    internal class Comparacao {
+
+        public int Numero { get; set; }
+
+        public Comparacao(int numero) {
+
+            Numero = numero;
+        }
+
+        public string numeroParOuImpar() {
+
+            if (Numero % 2 == 0) {
+                return "PAR";
+            } else {
+                return "IMPAR";
+            }
+        }
+        public override string ToString() {
+            return $"O número {Numero} é: {numeroParOuImpar()}";
+        }
+    }
+}
---
+++ 02 - Estruturas Condicionais (if-else)/Exercicio02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exercicio02.Entities;
+class Program {
+
+    public static void Main() {
+
+        Console.Write("Digite um número para verificar se ele é PAR ou IMPAR: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Comparacao c = new Comparacao(n);
+
+        Console.WriteLine();
+        Console.WriteLine(c);
+    }
+}
