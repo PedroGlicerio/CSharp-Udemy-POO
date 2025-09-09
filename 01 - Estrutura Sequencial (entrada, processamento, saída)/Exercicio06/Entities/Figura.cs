0bcd122ce709577234c18d11e5f2609e527e0d99
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-09 11:40:27 -0300

    Resolução do Exercício 06 - Estrutura Sequencial

---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio06/Entities/Figura.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exercicio06.Entities {
+    internal class Figura {
+
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public Figura(double a, double b, double c) {
+
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double areaTriangulo() {
+
+            return (A * C) / 2;
+        }
+
+        public double areaCirculo() {
+
+            double pi = 3.14159;
+            return pi * Math.Pow(C, 2);
+        }
+
+        public double areaTrapezio() {
+
+            return ((A + B) * C) / 2;
+        }
+
+        public double areaQuadrado() {
+
+            return Math.Pow(B, 2);
+        }
+
+        public double areaRetangulo() {
+
+            return A * B;
+        }
+
+        public override string ToString() {
+            return $"TRIANGULO: {areaTriangulo():f3}\nCIRCULO: {areaCirculo():f3}\nTRAPEZIO: {areaTrapezio():f3}\nQUADRADO: {areaQuadrado():f3}\nRETANGULO: {areaRetangulo():f3}";
+        }
+    }
+}
---
+++ 01 - Estrutura Sequencial (entrada, processamento, saída)/Exercicio06/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Exercicio06.Entities;
+
+class Program {
+    public static void Main() {
+
+        Console.Write("Digite três valores para fazermos o cálculo da área: ");
+        string[] valores = Console.ReadLine().Split(" ");
+        double a = double.Parse(valores[0], CultureInfo.InvariantCulture);
+        double b = double.Parse(valores[1], CultureInfo.InvariantCulture);
+        double c = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+        Figura f = new Figura(a, b, c);
+
+        Console.WriteLine();
+        Console.WriteLine(f);
+    }
+}
