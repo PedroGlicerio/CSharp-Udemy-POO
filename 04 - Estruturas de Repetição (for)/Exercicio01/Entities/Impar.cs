fa316fc47ac18d43fc7db4cf86f18ae2bb552bf4
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-11 11:03:04 -0300

    Resolução do Exercício 01 - Estrutura de Repeticao (for)

---
+++ 04 - Estruturas de Repetição (for)/Exercicio01/Entities/Impar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Exercicio01.Entities {
+    internal class Impar {
+
+        public int Numero { get; set; }
+
+        public void numerosImpares() {
+
+            Console.Write("Digite um número: ");
+            Numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Números Impares de 1 a {Numero}:");
+            for (int i = 1; i <= Numero; i += 2) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ 04 - Estruturas de Repetição (for)/Exercicio01/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using Exercicio01.Entities;
+class Program {
+    public static void Main() {
+
+        Impar impar = new Impar();
+        impar.numerosImpares();
+    }
+}
