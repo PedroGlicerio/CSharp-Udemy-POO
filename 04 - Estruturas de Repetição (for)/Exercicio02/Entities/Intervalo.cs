57f8e9cb432a9f8fc9438d232939af74dc58bf20
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-11 12:13:20 -0300

    Resolução do Exercício 02 - Estrutura de Repeticao (for)

---
+++ 04 - Estruturas de Repetição (for)/Exercicio02/Entities/Intervalo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercicio02.Entities {
+    internal class Intervalo {
+
+        public int Numero { get; set; }
+
+        public Intervalo(int numero) {
+
+            Numero = numero;
+        }
+        public void dentroIntervalo() {
+
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 0; i < Numero; i++) {
+
+                Console.Write("Digite um valor: ");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n >= 10 && n <= 20) {
+                    dentro++;
+                } else {
+                    fora++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Total de valores dentro e fora do intervalo [10,20]:");
+            Console.WriteLine($"{dentro} dentro");
+            Console.WriteLine($"{fora} fora");
+        }
+    }
+}
---
+++ 04 - Estruturas de Repetição (for)/Exercicio02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Exercicio02.Entities;
+class Program {
+    public static void Main() {
+
+        Console.Write("Digite o total de número que serão arquivados: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Intervalo intervalo = new Intervalo(N);
+
+        Console.WriteLine();
+        intervalo.dentroIntervalo();
+    }
+}
