c34bc11522d52df7c52b1fa1685d3cfdc6214be3
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-09-13 16:12:38 -0300

    Resolução do Exercício 03 - Estrutura de Repeticao (for)

---
+++ 04 - Estruturas de Repetição (for)/Exercicio03/Entities/Media.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio03.Entities {
+    internal class Media {
+
+        public int Numero { get; set; }
+
+        public Media(int numero) {
+
+            Numero = numero;
+        }
+
+        public void numeroMedia() {
+
+            for (int i = 0; i < Numero; i++) {
+
+                Console.Write($"Digite os valores: ");
+                string[] n = Console.ReadLine().Split(" ");
+                double valor1 = double.Parse(n[0], CultureInfo.InvariantCulture);
+                double valor2 = double.Parse(n[1], CultureInfo.InvariantCulture);
+                double valor3 = double.Parse(n[2], CultureInfo.InvariantCulture);
+
+                
+                double media = (valor1 * 2 + valor2 * 3 + valor3 * 5) / 10;
+                Console.WriteLine($"Resultado: {media:f1}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 04 - Estruturas de Repetição (for)/Exercicio03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Exercicio03.Entities;
+class Program {
+    public static void Main() {
+
+        Console.Write("Digite um valor: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Media media = new Media(N);
+
+        Console.WriteLine();
+        media.numeroMedia();
+        Console.WriteLine();
+    }
+}
