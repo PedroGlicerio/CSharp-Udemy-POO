97db4be807b7d25af8c61a11dca30ba55924a30c
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-10-22 11:46:17 -0300

    Resolução do Exercício 03 - Vetores

---
+++ 06 - Vetores/Exercicio03/Entities/Estudante.cs
@@ -0,0 +1,15 @@
+﻿namespace Teste.Entities {
+    internal class Estudante {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Estudante(string nome, string email, int quarto) {
+
+            Nome = nome;
+            Email = email;
+            Quarto = quarto;
+        }
+    }
+}
---
+++ 06 - Vetores/Exercicio03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Teste.Entities;
+class Program {
+
+    public static void Main(string[] args) {
+
+        Console.Write("Quantos quartos você gostaria de alugar: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Estudante[] estudantes = new Estudante[10];
+
+        for (int i = 0; i < N; i++) {
+
+            Console.WriteLine();
+            Console.WriteLine($"Aluguel #{i + 1}:");
+            Console.WriteLine("============");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Quarto: ");
+            int quarto = int.Parse(Console.ReadLine());
+
+            estudantes[quarto] = new Estudante(nome, email, quarto);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Quartos ocupados:");
+        for (int i = 0; i < estudantes.Length; i++) {
+            if (estudantes[i] != null) {
+                Console.WriteLine($"{estudantes[i].Quarto}: {estudantes[i].Nome}, {estudantes[i].Email}");
+            }
+        }
+    }
+}
