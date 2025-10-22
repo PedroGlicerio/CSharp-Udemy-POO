e0ecc4e53bb7967f6fe40fe258d7517f20189c78
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-10-22 11:40:05 -0300

    Resolução do Exercício 01 - Vetores

---
+++ 06 - Vetores/Exercicio01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+class Program {
+
+    public static void Main(string[] args) {
+
+        Console.Write("Digite um valor N para quantos números deseja salvar: ");
+        int N = int.Parse(Console.ReadLine());
+
+        double[] vect = new double[N];
+
+        Console.WriteLine();
+
+        for (int i = 0; i < N; i++) {
+
+            Console.Write($"Digite o {i + 1}° valor: ");
+            vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        }
+
+        double soma = 0;
+        foreach (double numeros in vect) {
+            soma += numeros;
+        }
+
+        double media = soma / N;
+
+        Console.WriteLine();
+        Console.WriteLine($"A média dos {N} números digitados foi: {media.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
