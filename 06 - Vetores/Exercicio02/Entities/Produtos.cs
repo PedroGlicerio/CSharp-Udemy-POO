2b0e8bafcedb82232c5e2e026b2bf4dde77b913c
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-10-22 11:43:18 -0300

    Resolução do Exercício 02 - Vetores

---
+++ 06 - Vetores/Exercicio02/Entities/Produtos.cs
@@ -0,0 +1,13 @@
+﻿namespace Teste.Entities {
+    internal class Produtos {
+
+        public string Nome { get; set; }
+        public double Preco { get; private set; }
+
+        public Produtos(string nome, double preco) {
+
+            Nome = nome;
+            Preco = preco;
+        }
+    }
+}
---
+++ 06 - Vetores/Exercicio02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using Teste.Entities;
+class Program {
+
+    public static void Main(string[] args) {
+
+        Console.Write("Digite quantos produtos você deseja passar: ");
+        int N = int.Parse(Console.ReadLine());
+
+        Produtos[] produtos = new Produtos[N];
+
+        for (int i = 0; i < N; i++) {
+
+            Console.WriteLine();
+            Console.Write($"Digite o nome do {i + 1}° produto: ");
+            string nome = Console.ReadLine();
+            Console.Write($"Digite o preço do {i + 1}° produto: R$");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            produtos[i] = new Produtos(nome, preco);
+        }
+
+        double soma = 0;
+        foreach (Produtos p in produtos) {
+            soma += p.Preco;
+        }
+
+        double media = soma / N;
+
+        Console.WriteLine();
+        Console.Write($"A média dos preços de {N} produtos foi: R${media.ToString("F2", CultureInfo.InvariantCulture)}");
+    }
+}
