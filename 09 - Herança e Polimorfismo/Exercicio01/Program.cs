70f510670c5876bb237ece8737e9df1f47be30e0
Pedro Glicerio <pedro.glicerio1204@gmail.com>
2025-10-28 22:41:34 -0300

    Resolução do Exercício 01 - Heranca e Polimorfismo (Refeito)

--- 09 - Herança e Polimorfismo/Exercicio01/Program.cs
+++ 09 - Herança e Polimorfismo/Exercicio01/Program.cs
@@ -15,26 +15,19 @@ class Program {
             Console.WriteLine($"Funcionário #{i + 1} dados:");
             Console.Write("Funcionário terceirizado [S/N]: ");
             char opc = char.Parse(Console.ReadLine().ToUpper());
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Horas: ");
+            int horas = int.Parse(Console.ReadLine());
+            Console.Write("Valor por hora: ");
+            double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             if (opc == 'N') {
-
-                Console.Write("Nome: ");
-                string nome = Console.ReadLine();
-                Console.Write("Horas: ");
-                int horas = int.Parse(Console.ReadLine());
-                Console.Write("Valor por hora: ");
-                double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
                 list.Add(new Funcionario(nome, horas, valorHora));
 
             } else {
 
-                Console.Write("Nome: ");
-                string nome = Console.ReadLine();
-                Console.Write("Horas: ");
-                int horas = int.Parse(Console.ReadLine());
-                Console.Write("Valor por hora: ");
-                double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
                 Console.Write("Despesa adicional: ");
                 double despesa = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
